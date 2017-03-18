edc31cc226078c54de4c5db4bc5f7d31129ae84a
BrianMunoz <brian.munoz1@upr.edu>
2017-03-18 18:50:32 -0400

    Bombs set and use of flag!

--- Lab5/src/MyMouseAdapter.java
+++ Lab5/src/MyMouseAdapter.java
@@ -7,9 +7,16 @@ import java.util.Random;
 
 import javax.swing.JFrame;
 
-
 public class MyMouseAdapter extends MouseAdapter {
 	private Random generator = new Random();
+	public int [] posXBomb = new int [10];
+	public int [] posYBomb = new int [10];
+	public Color bombs = Color.BLACK;
+	public Color uncoveredCell = Color.LIGHT_GRAY;
+	public Color coveredCell = Color.WHITE;
+	public boolean bomb = false;
+	public int useFlag = 1;
+	
 	public void mousePressed(MouseEvent e) {
 		switch (e.getButton()) {
 		case 1:		//Left mouse button
@@ -84,6 +91,26 @@ public class MyMouseAdapter extends MouseAdapter {
 			myPanel.y = y;
 			int gridX = myPanel.getGridX(x, y);
 			int gridY = myPanel.getGridY(x, y);
+			
+			int quantityCol;
+			int quantityRow;
+			
+			//Create bombs coordinates (BUG: can have similar coordinate. FIX!!!!!)
+			if(bomb == false){
+			for(int i = 1; i <= posXBomb.length; i++){
+				do{
+					quantityCol = generator.nextInt(10);
+					quantityRow = generator.nextInt(10);
+				}while(quantityCol == 0 || quantityRow == 0 || myPanel.colorArray[quantityCol][quantityRow].equals(bombs));
+				posXBomb[i - 1] = quantityCol;
+				posYBomb[i - 1] = quantityRow;
+				bomb = true;
+				System.out.print(posXBomb[i-1] + "");
+				System.out.print(posYBomb[i-1]);
+				System.out.print(" ");
+				}
+			}
+			
 			if ((myPanel.mouseDownGridX == -1) || (myPanel.mouseDownGridY == -1)) {
 				//Had pressed outside
 				//Do nothing
@@ -208,33 +235,27 @@ public class MyMouseAdapter extends MouseAdapter {
 							}
 							}else {
 							//On the grid other than on the left column and on the top row:
-							Color newColor = null;
-							do{
-							switch (generator.nextInt(5)) {
-							case 0:
-								newColor = Color.YELLOW;
-								break;
-							case 1:
-								newColor = Color.MAGENTA;
-								break;
-							case 2:
-								newColor = Color.BLACK;
-								break;
-							case 3:
-								newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-								break;
-							case 4:
-								newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-								break;
+								
+								for(int posArray = 0; posArray < posXBomb.length; posArray++){
+									for(int i = 1; i <= 1; i++){
+										if(myPanel.colorArray[gridX][gridY].equals(coveredCell)){
+											if(gridX == posXBomb[posArray] && gridY == posYBomb[posArray]){
+												myPanel.colorArray[gridX][gridY] = bombs;
+												myPanel.repaint();
+											}
+										}
+									}
+								}
+								
+								if(myPanel.colorArray[gridX][gridY].equals(coveredCell)){
+									myPanel.colorArray[gridX][gridY] = uncoveredCell;
+									myPanel.repaint();
+								}
+								
 							}
-							}while(myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY].equals(newColor));
-							myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
-							myPanel.repaint();
 						}
 					}
 				}
-			}
-			myPanel.repaint();
 			break;
 		case 3:		//Right mouse button
 			Component d = e.getComponent();
@@ -246,41 +267,38 @@ public class MyMouseAdapter extends MouseAdapter {
 			}
 			JFrame frame = (JFrame) d;
 			MyPanel panel = (MyPanel) frame.getContentPane().getComponent(0);
-			Insets outsideInSets = frame.getInsets();
-			int x1Outside = outsideInSets.left;
-			int y1Outside = outsideInSets.top;
-			e.translatePoint(-x1Outside, -y1Outside);
-			int xOutside = e.getX();
-			int yOutside = e.getY();
-			panel.x = xOutside;
-			panel.y = yOutside;
-			int gridXOutside = panel.getGridX(xOutside, yOutside);
-			int gridYOutside = panel.getGridY(xOutside, yOutside);
-			
-			if(gridXOutside == -1 || gridYOutside == -1){
-				
-				Color colorOutside = null;
-				for(int i = 1; i <= 9; i++){
-					for(int j = 1; j <= 9; j++){
-						do{
-							switch (generator.nextInt(3)){
-							
-							case 0:
-								colorOutside = Color.ORANGE;
-								break;
-							case 1:
-								colorOutside = Color.CYAN;
-								break;
-							case 2:
-								colorOutside = Color.PINK;
-								break;
-							}
-						}while(panel.colorArray[gridXOutside + i + 1][gridYOutside + j + 1].equals(colorOutside));
-						panel.colorArray[gridXOutside + i + 1][gridYOutside + j + 1] = colorOutside;
-						panel.repaint();
-						}
-					}
+			Insets flagInSets = frame.getInsets();
+			int x1Flag = flagInSets.left;
+			int y1Flag = flagInSets.top;
+			e.translatePoint(-x1Flag, -y1Flag);
+			int xFlag = e.getX();
+			int yFlag = e.getY();
+			panel.x = xFlag;
+			panel.y = yFlag;
+			int gridXFlag = panel.getGridX(xFlag, yFlag);
+			int gridYFlag = panel.getGridY(xFlag, yFlag);
+			Color flag = Color.RED;
+			if(useFlag == 1){
+				if(gridXFlag != -1 && gridXFlag != 0 & gridYFlag != -1 && gridYFlag != 0 && panel.colorArray[gridXFlag][gridYFlag].equals(coveredCell)){
+					panel.colorArray[gridXFlag][gridYFlag] = flag;
+					panel.repaint();
+					useFlag = 2;
+				}else if(gridXFlag != -1 && gridXFlag != 0 & gridYFlag != -1 && gridYFlag != 0 && panel.colorArray[gridXFlag][gridYFlag].equals(flag)){
+					panel.colorArray[gridXFlag][gridYFlag] = coveredCell;
+					panel.repaint();
+					useFlag = 1;
 				}
+			}else if(useFlag == 2){
+				if(gridXFlag != -1 && gridXFlag != 0 & gridYFlag != -1 && gridYFlag != 0 && panel.colorArray[gridXFlag][gridYFlag].equals(flag)){
+				panel.colorArray[gridXFlag][gridYFlag] = coveredCell;
+				panel.repaint();
+				useFlag = 1;
+				}else if(gridXFlag != -1 && gridXFlag != 0 & gridYFlag != -1 && gridYFlag != 0 && panel.colorArray[gridXFlag][gridYFlag].equals(coveredCell)){
+					panel.colorArray[gridXFlag][gridYFlag] = flag;
+					panel.repaint();
+					useFlag = 2;
+				}
+			}
 			
 			break;
 		default:    //Some other button (2 = Middle mouse button, etc.)
@@ -288,4 +306,10 @@ public class MyMouseAdapter extends MouseAdapter {
 			break;
 		}
 	}
-}
+	
+	
+	
+	}
+
+
+	
